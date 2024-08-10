bb1024fd7d4fa601e25d85e2e2d47f0975578300
Honchar Denys <crackeraki@gmail.com>
2024-08-10 21:16:40 +0300

    added appId to translate

--- router.api.js
+++ router.api.js
@@ -4,6 +4,7 @@ module.exports = async (waw) => {
 		translate: String,
 		slug: String,
 		lang: String,
+		appId: String,
 	});
 
 	const Translates = waw.mongoose.model("Translate", TranslateSchema);
@@ -13,6 +14,7 @@ module.exports = async (waw) => {
 		word: String,
 		page: String,
 		description: String,
+		appId: String,
 	});
 
 	const Word = waw.mongoose.model("Word", WordSchema);
@@ -20,13 +22,20 @@ module.exports = async (waw) => {
 	const routerTranslate = waw.router("/api/translate");
 
 	waw.translate = (req) => {
-		return (obj) => {};
+		return (obj) => { };
 	};
 
-	waw.translates = async (lang) => {
-		const translates = await Translates.find({
-			lang,
-		});
+	waw.translates = async (lang, appId = "") => {
+		const translates = await Translates.find(
+			appId
+				? {
+					lang,
+					appId,
+				}
+				: {
+					lang,
+				}
+		);
 
 		const obj = {};
 
@@ -38,7 +47,7 @@ module.exports = async (waw) => {
 	};
 
 	const translates = {};
-	waw.config.languages = waw.config.languages || ['en'];
+	waw.config.languages = waw.config.languages || ["en"];
 	for (const lang of waw.config.languages) {
 		translates[lang] = await waw.translates(lang);
 	}
@@ -51,8 +60,7 @@ module.exports = async (waw) => {
 	waw.translate = (req) => {
 		let lang = req.session.language ? req.session.language : "en";
 		if (!req.session.language) {
-			const ip =
-				req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+			const ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
 
 			const geo = geoip.lookup(ip);
 
@@ -78,26 +86,37 @@ module.exports = async (waw) => {
 		};
 	};
 
-	routerTranslate.get("/get", async (req, res) => {
-		const translates = await Translates.find({});
+	const get = async (req, res) => {
+		const translates = await Translates.find(
+			req.params.appId ? { appId: req.params.appId } : {}
+		);
 
 		const obj = {};
 
 		for (var i = 0; i < translates.length; i++) {
 			if (!obj[translates[i].lang]) obj[translates[i].lang] = {};
 
-			obj[translates[i].lang][translates[i].slug] =
-				translates[i].translate;
+			obj[translates[i].lang][translates[i].slug] = translates[i].translate;
 		}
 
 		res.json(obj);
-	});
+	};
+	routerTranslate.get("/get", get);
+	routerTranslate.get("/get/:appId", get);
 
-	routerTranslate.post("/create", waw.role("admin"), async (req, res) => {
-		const translate = await Translates.findOne({
-			slug: req.body.slug,
-			lang: req.body.lang,
-		});
+	routerTranslate.post("/create", async (req, res) => {
+		const translate = await Translates.findOne(
+			req.body.appId
+				? {
+					slug: req.body.slug,
+					lang: req.body.lang,
+					appId: req.body.appId,
+				}
+				: {
+					slug: req.body.slug,
+					lang: req.body.lang,
+				}
+		);
 
 		translates[req.body.lang] = translates[req.body.lang] || {};
 		translates[req.body.lang][req.body.slug] = req.body.translate;
@@ -115,25 +134,43 @@ module.exports = async (waw) => {
 	});
 
 	routerTranslate.post("/delete", waw.role("admin"), async (req, res) => {
-		await Translates.deleteMany({
-			slug: req.body.slug,
-		});
+		await Translates.deleteMany(
+			req.body.appId
+				? {
+					appId: req.body.appId,
+					slug: req.body.slug,
+				}
+				: {
+					slug: req.body.slug,
+				}
+		);
 
 		res.json(true);
 	});
 
 	const routerWord = waw.router("/api/word");
 
-	routerWord.get("/get", async (req, res) => {
-		const words = await Word.find({});
+	const getWord = async (req, res) => {
+		const words = await Word.find(
+			req.params.appId ? { appId: req.params.appId } : {}
+		);
 
 		res.json(words || []);
-	});
+	};
+	routerWord.get("/get", getWord);
+	routerWord.get("/get/:appId", getWord);
 
-	waw.word = async (slug) => {
-		const word = await Word.findOne({
-			slug,
-		});
+	waw.word = async (slug, appId = "") => {
+		const word = await Word.findOne(
+			appId
+				? {
+					appId,
+					slug,
+				}
+				: {
+					slug,
+				}
+		);
 
 		if (word) {
 			return word;
@@ -145,19 +182,26 @@ module.exports = async (waw) => {
 				slug,
 				word,
 				page,
+				appId,
 			});
 		}
 	};
 
 	routerWord.post("/create", async (req, res) => {
-		const word = await waw.word(req.body.slug);
-		res.json();
+		res.json(await waw.word(req.body.slug, req.body.appId || ""));
 	});
 
 	routerWord.post("/delete", waw.role("admin"), async (req, res) => {
-		await Word.deleteOne({
-			_id: req.body._id,
-		});
+		await Word.deleteOne(
+			req.body.appId
+				? {
+					appId: req.body.appId,
+					_id: req.body._id,
+				}
+				: {
+					_id: req.body._id,
+				}
+		);
 
 		res.json(true);
 	});
